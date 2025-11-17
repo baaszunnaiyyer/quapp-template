1d2a703c7a53bd04ebbb09093e3977e0974987f1
Baas Zunnaiyyer <baaszunnaiyyer@gmail.com>
2025-11-17 22:32:56 +0500

    Updated dev and Common Dependecies

---
+++ solid-js/src/App.jsx
@@ -0,0 +1,21 @@
+import quappLogo from '/quapp.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={quappLogo} class="logo" alt="Quapp logo" />
+      </div>
+      <h1>Quapp</h1>
+      <p>Build your Native apps With a Snap of a Finger</p>
+      <div class="card">
+        <p>
+          Edit <code>src/App.jsx</code> to see Changes in your App
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
